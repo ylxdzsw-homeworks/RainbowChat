27b0174ac43a180983dfd407733649cd6ac3cb16
purebluesong <mousecatchcat@yeah.net>
2015-07-08 23:47:46 +0800

    增加了几个命令，但是注册一直注册不了，返回500错误

--- SublimeClient/rainbow.py
+++ SublimeClient/rainbow.py
@@ -19,6 +19,13 @@ user = UserInfo('Admin', 'Admin')
 help_message = 'Please input  \'!help\'  and press enter if you want more information'
 plugin_name = 'rainbowchat'
 file_suffix = 'rainbowchat'
+
+server_url = 'http://localhost:3000'
+server_url_user = server_url+'/user'
+server_url_msg = server_url+'/msg'
+server_url_login = server_url+'/auth'
+header = {'Content-Type':'application/json'}
+
 welcome_info = 'Welcome To RainBowChat!\n'
 version = 'rainbowchat version:' + '0.0.0'
 help_format = '\'!help\''
@@ -40,32 +47,40 @@ class RainbowCommand(sublime_plugin.TextCommand):
 def show_input_panel(caption = None, content=''):
     if caption == None:
         caption = user.username
-    sublime.active_window().show_input_panel(caption, content, input, None, None)
+    if username == None:
+        show_login_panel()
+    else:
+        sublime.active_window().show_input_panel(caption, content, input, None, None)
 
 
 def show_login_panel():
-    init_text = 'format:login:'+login_format +'    register:'+ register_format
+    init_text = 'format:login:'+login_format +'    register:'+ register_format +'>>>'
     sublime.active_window().show_input_panel('Login:', init_text, login_register_done, login_register_onchange, None)
 
 
 def login_register_done(text):
+    if len(text.split('>>>'))>1 :
+        text = text.split('>>>')[1]
     infos = text.split('@')
-    if len(infos) == 3:
+    sublime.status_message(str(len(infos)))
+    if len(infos) == 3 and infos[2]=='new':
         register(infos[0], infos[1])
-    elif len(text) == 2:
+    elif len(infos) == 2:
         login(infos[0], infos[1])
     else:
-        sublime.active_window().error_message('input error!')
+        sublime.error_message('input error!'+infos[0])
         show_login_panel()
 
 
 def login_register_onchange(text):
+    if len(text.split('>>>'))>1 :
+        text = text.split('>>>')[1]
     if len(text.split('@'))==2 :
-        sublime.status_message('it is a correct login format')
+        sublime.status_message('It is a correct login format')
     elif len(text.split('@'))==3 and text.split('@')[2]=='new':
-        sublime.status_message('it is a correct register format')
+        sublime.status_message('It is a correct register format')
     else:
-        sublime.status_message('it is not a correct format:the correct format:'+login_format+'or'+register_format)
+        sublime.status_message('The correct format:'+login_format+'or'+register_format)
 
 
 def checklogin():
@@ -73,25 +88,55 @@ def checklogin():
 
 
 def register(username, password):
-    user.username = username
-    user.password = password
-    sublime.status_message("register succeed!your name:" + username)
-    show_input_panel(user.username, 'input message here')
+    user_data = {'username':username, 'password':password}
+    if send_register_info_to_server(user_data) == None:
+        show_login_panel()
+        sublime.status_message('Due to something ,register failed!')
+    else:
+        user.username = username
+        user.password = password    
+        show_input_panel(user.username+':', 'input message here')
+
+
+def send_register_info_to_server(user_data): 
+    post_data = json.dumps(user_data)
+    req = urllib2.Request(server_url_user, post_data, header)
+    try:
+        res = urllib2.urlopen(req)
+        if res.getcode()==200 :
+            sublime.message_dialog('Congratulations! Register succeed!')
+            return content
+    except urllib2.HTTPError, e:
+        sublime.message_dialog('Register failed, error:' + str(e))
+        return None
+
 
 
 def login(username, password):
-    if check_user():
+    if check_user(username,password):
+        sublime.status_message('Login succeed!')
+        user.login_state = True
         user.username = username
         user.password = password
-        user.login_state = True
         show_input_panel()
-    else:
+    else :
         show_login_panel()
-        sublime.active_window().message_dialog('login failed,maybe username or password is wrong')
 
 
-def check_user():
-    return True
+def check_user(username, password):
+    user_data = {'username':username, 'password':password}
+    post_data = json.dumps(user_data)
+    req = urllib2.Request(server_url_login, post_data, header)
+    try:
+        res = urllib2.urlopen(req)
+        if res.getcode()==200 :
+            return True
+        else :
+            return False
+    except urllib2.URLError, e:
+        sublime.error_message('Login failed, error:' + str(e))
+        return False
+    
 
 
 def input(text):
@@ -122,14 +167,30 @@ def check_chatview():
 def command(commandstr):
     commands = commandstr.strip().split(' ')
     if commands[0] == 'chatwith':
-        if len(commands) == 1:
-            sublime.error_message("you must point out one person at least!")
-            show_input_panel()
-        elif commands[1] != ' ':
-            startchat(commands[1])
+        cahtwith(commands)
     elif commands[0] == 'help':
         help()
         show_input_panel()
+    elif commands[0] == 'logout':
+        logout()
+    elif commands[0] == 'relogin':
+        relogin()
+
+
+def relogin():
+    logout()
+    show_login_panel()
+
+def chatwith(commands):
+    if len(commands) == 1:
+        sublime.error_message("you must point out one person at least!")
+        show_input_panel()
+    elif commands[1] != ' ':
+        startchat(commands[1])
+
+def logout():
+    sublime,status_message('Logout succeed!')
+    pass
 
 
 def startchat(username):
